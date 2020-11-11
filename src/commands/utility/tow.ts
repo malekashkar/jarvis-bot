82905fcced46f0bc811af85388ecb8ffbbaa5a90
Dep0sit <ashkarmalek0@gmail.com>
2020-11-11 16:59:40 -0500

    :bug: Fixed the start script

--- src/commands/utility/tow.ts
+++ src/commands/utility/tow.ts
@@ -12,7 +12,6 @@ import {
   uploadImage,
   toTitleCase,
 } from "../../util";
-import spreadsheetCreds from "../../../spreadsheet_creds.json";
 import settings from "../../settings";
 import embeds from "../../util/embed";
 import Command from "..";
@@ -142,7 +141,11 @@ export default class TowCommand extends Command {
 
 async function submitInformation(information: Information) {
   const doc = new GoogleSpreadsheet(settings.spreadsheet);
-  await doc.useServiceAccountAuth(spreadsheetCreds);
+  await doc.useServiceAccountAuth({
+    private_key:
+      "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCyKiS31HSPenjh\nbVlKLEOJp8zxAByXNlX1cV+mAqWzT7wAsVmfWQ36r78wJ7+w4/o5oRIlDp8CJ2qc\nXE940zPHeqKRxfsmCCxT0B4icnjEM+BYC1AQ7nOesfqla8rpqADEKMfc8GGavaNj\nH4fH+zMpKlGxYGF0iuha5EJZcLKA92vDWROEblljWWXp+//Gxf89fMq7k+/mBHtB\nNbflQae6zcMXyNFIPHCsu9Ue3hastBL488vSevS5/UEQxCT4Vsjb7m//uEza8glC\nxnIglMPmuINDO9J2lzFSNohWkqUQIknmpBXkJKrMmFaMnYk3f44hf6B30Jumc+VD\nV+fUKPAlAgMBAAECggEAC/jEfFzIk06PulgacJ8+T4zousX+5dNHHeXHH1TctQqV\nz7cbFlfJ1m9CSdZV7Wk4SnCOE1Li8OZcpdKXHX+VomEVdPWofWL9tYkNyVNHVanF\nuKzT7btUJhUACmcOnLpHCOdTnL9DU75L8RBwk4nH44t+L0zSZ54g5GCjq7ZkrEE3\nFMZ9/HJYq0svZwnxHHM0iSrNF4m/ID1aCHCrO8kCeKG6VXtXjHPaJDGjYQkzqfP1\nPeCCiNJ0KF52hI6k5p4M5wO2R6Aw9Vz/V96imkO9wz+aFaorlMUPLTDxSNRXEC/u\nW00fYQrlNTWNtzyrOjgshkwm3IdIdqzRRh8k5KrKCQKBgQDrRa//um28c5T60Ckm\nXXfEZ5ayyRJu5rUhXFEpuSr9ZYhgSM39MFTbzOEUa9r5s4MqFTvC4eJv+B8GZUiA\n6qTnref/Txd/rogxrBrRq3/AjywEGTrS71qOcSHobJe7WxGvkSZv14Xwn1iqKuXO\n3NW03sY/GWtiCy/QrdAqKJ7DPQKBgQDB3HSVS/s6pCy5rsR/FIBTfbUDzJzuy6G1\nb3uhjCcLM8ow7vE8Ig3xgWq6Q/mNp2CYj3EIECXTJPoCKgNS8Zrx/aNOvFB8oMhE\nGaTIh+Wef2RO1zVvB4qJX6gbj1+tSq7hRViFS/QnESQffuvcMZHXI/ZACK99TdFB\n8gJIaemPCQKBgQCqj3e7JdUeFJDBj5z1YjdMXGikrh495eUUDVANtl6TG0NVwhIL\nZjluq0XVPyeddGU3YfNUIdu9npwnT8/THch06N9SeG0ptEap9AHxVKol1+VqBzxc\n5Qa6Uwvo6qjJO4lDxS+fTxiby3lEzBqT8AbBu+runaf6iO+eEhPiLFaEZQKBgFtf\nt6uvFok7XMFgeMmVNi8kLy/NP6LmZCAKnfE1CColjQWnehjnzD6qy01kQLndzI/b\n/GDai5mL8jMW7l4n9upcrJ6t8BnuBrbih2LlevtL/FGU6mCc41WAutHSsDwT2X3g\nm6qodDn9wFMAFdfBnYYUMztXXK/o/9rfYOgU07tRAoGAfqe0iZqb2BzWixZrvSXR\nsoS2TXFta5lmDGD67xmmGDaSSGr5nphvLmEPRneZy+GzjROR06FFzAq/pr8p8jsg\nXLZ/ixzMNfewZlFq1TncdQQdK43gkqsMxBJPcUQhP4vHM68egJBuOBmCqZm/2/c8\nbtLrGWUPcbJsIqm8LAGhc9U=\n-----END PRIVATE KEY-----\n",
+    client_email: "sheets@arland-auto-sheet-bot.iam.gserviceaccount.com",
+  });
   await doc.loadInfo();
 
   const sheet = doc.sheetsByIndex[5];
